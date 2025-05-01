8f9b465fd3e49b7737836874afdb94fcba845a1b
Harihuynh2007 <hminhhai2000@gmail.com>
2025-05-01 11:00:52 +0700

    update

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,4 +1,5 @@
 import { Schema, model } from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const userSchema = new Schema({
   name: { type: String, required: true },
@@ -7,6 +8,19 @@ const userSchema = new Schema({
   progress: { type: Number, default: 0 },  // Ví dụ: Lộ trình học tập của user
 }, { timestamps: true });
 
-const User = model('User', userSchema);
+// Hash mat khai truoc khi luu vao DB
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+  
 
+// so sanh mat khau khi dang nhap
+userSchema.methods.comparePassword = async function (enteredPassword: string) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = model('User', userSchema);
 export default User;
