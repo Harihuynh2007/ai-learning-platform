32ce3acec48c20db10c3d9f44c69c26567a742a0
Harihuynh2007 <hminhhai2000@gmail.com>
2025-05-01 12:40:37 +0700

    delete

--- backend/src/routes/user.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { createUser, getUsers } from "../controllers/user.controller";
-
-const router = Router();
-
-router.post('/users', createUser); // Tao user moi
-router.get('/users', getUsers); // Lay tat ca users
-
-export default router;
